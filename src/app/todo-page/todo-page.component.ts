49a88cc30bd490e8feb50d94c77978476d9e94c4
Jason14468 <chenyenting08@gmail.com>
2023-12-29 14:51:47 +0800

    feat(nav): 建立導覽列元件

---
+++ src/app/todo-page/todo-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-todo-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './todo-page.component.html',
+  styleUrl: './todo-page.component.css'
+})
+export class TodoPageComponent {
+
+}
