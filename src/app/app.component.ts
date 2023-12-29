f5d532607f1f0202174e42ed632910d36d2eadfb
Jason14468 <chenyenting08@gmail.com>
2023-12-29 15:24:05 +0800

    feat(home): 待辦事項頁面路由組態設定

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
---
+++ src/app/todo-detail-page/todo-detail-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-todo-detail-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './todo-detail-page.component.html',
+  styleUrl: './todo-detail-page.component.css'
+})
+export class TodoDetailPageComponent {
+
+}
